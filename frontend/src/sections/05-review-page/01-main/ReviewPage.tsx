74f8599ca7f78adb2eaa4969da810ebb50437acd
simfrisk <simoncarlfrisk@gmail.com>
2025-06-20 21:10:34 +0200

    Added Desktop view

--- frontend/src/sections/05-review-page/01-main/ReviewPage.tsx
+++ frontend/src/sections/05-review-page/01-main/ReviewPage.tsx
@@ -3,24 +3,59 @@ import { CommentForm } from "../components/comment-form/CommentForm";
 import { CommentSection } from "../components/comment-section/01-main/CommentSection";
 import { VideoSection } from "../components/video-section/VideoSection";
 import { CommentHeader } from "../components/comment-header/CommentHeader";
-
+import { MediaQueries } from "../../../themes/mediaQueries";
 
 export const ReviewPage = () => {
   return (
-    <>
-    {/* <Options /> */}
     <Container>
-      <VideoSection />
-      <CommentHeader />
-      <CommentSection />
-      <CommentForm />
+      <StyledVideoSection />
+      <RightColumn>
+        <StyledCommentHeader />
+        <StyledCommentSection />
+      </RightColumn>
+      <StyledCommentForm />
     </Container>
-    </>
-  )
+  );
 };
+const Container = styled.div`
+  display: grid;
+  width: 100%;
 
 
-const Container = styled.div `
-display: grid;
-width: 100%;
-`
+  @media ${MediaQueries.biggerSizes} {
+    grid-template-columns: 1fr 400px;
+    grid-template-areas:
+      "video right"
+      "form right";
+    gap: 8px;
+    align-items: start;
+  }
+`;
+
+
+const StyledVideoSection = styled(VideoSection)`
+  grid-area: video;
+`;
+
+const StyledCommentForm = styled(CommentForm)`
+  grid-area: form;
+`;
+
+const RightColumn = styled.div`
+@media ${MediaQueries.biggerSizes} {
+  grid-area: right;
+  display: flex;
+  flex-direction: column;
+  background-color: #f5f5f5;
+  gap: 6px;
+  height: 100vh;
+}
+`;
+
+const StyledCommentHeader = styled(CommentHeader)`
+  /* No grid-area needed now, it's inside RightColumn */
+`;
+
+const StyledCommentSection = styled(CommentSection)`
+  /* Same here */
+`;
