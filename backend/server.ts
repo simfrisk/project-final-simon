4f2634781609f4e3dbe6a2c20f800399adb23199
simfrisk <simoncarlfrisk@gmail.com>
2025-06-25 04:02:48 +0200

    Added typescript and removed babel

--- backend/server.ts
+++ backend/server.ts
@@ -1,7 +1,7 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-import { getHome } from "./endpoints/getHome";
+import { getHome } from "./endpoints/getHome.ts";
 
 const mongoUrl: string = process.env.MONGO_URL || "mongodb://localhost/final-project";
 mongoose.connect(mongoUrl);
