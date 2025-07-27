1cc7bc9d03c64decf97d5688e0e7a9bdd7b0e9c1
simfrisk <simoncarlfrisk@gmail.com>
2025-07-27 11:58:35 +0200

    added  cloudinary backend setp 1

---
+++ backend/middleware/uploadImage.ts
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+dotenv.config()
+
+import multer from "multer";
+import { v2 as cloudinary } from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME!,
+  api_key: process.env.CLOUDINARY_API_KEY!,
+  api_secret: process.env.CLOUDINARY_API_SECRET!,
+});
+
+const imageStorage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "profile_pictures",
+    resource_type: "image",
+    allowed_formats: ["jpg", "jpeg", "png", "webp"],
+  } as any,
+});
+
+export const uploadImage = multer({ storage: imageStorage });
