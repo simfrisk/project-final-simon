ce93c6d67ecccade5f98d35667411acf8f958496
simfrisk <simoncarlfrisk@gmail.com>
2025-08-02 20:48:50 +0200

    reply model

--- backend/models/Reply.ts
+++ backend/models/Reply.ts
@@ -4,12 +4,16 @@ export interface ReplyType extends Document {
   content: string;
   commentId: Types.ObjectId;
   createdAt: Date;
+  isChecked: boolean;
+  replyCreatedBy: Types.ObjectId;
 }
 
 const ReplySchema = new Schema<ReplyType>({
   content: { type: String, required: true },
   commentId: { type: Schema.Types.ObjectId, ref: "Comment", required: true },
   createdAt: { type: Date, default: Date.now },
+  isChecked: { type: Boolean, required: true },
+  replyCreatedBy: { type: Schema.Types.ObjectId, ref: "User", required: true }
 });
 
 export const Reply = model<ReplyType>("Reply", ReplySchema);
